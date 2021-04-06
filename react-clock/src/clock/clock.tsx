20b57c614a18373f0e013ad7d7e383327c687279
Ahasan Kabir <kabirahasaan@gmail.com>
2021-04-06 13:44:32 -0400

    add: clock component

---
+++ react-clock/src/clock/clock.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import { Card } from 'react-bootstrap';
+
+
+const Clock = () =>{
+
+    const [hour, setHour] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    setTimeout(() =>{
+        const date = new Date();
+        setHour(date.getHours());
+        setMinutes(date.getMinutes());
+        setSeconds(date.getSeconds());
+    },1000);
+
+    return(
+        <Card>
+           <Card.Body>
+               <Card.Title 
+               style={{textAlign: "center"}}>Time Now
+               </Card.Title>
+               <Card.Subtitle>
+                  { `${hour} Hour: ${minutes} Minutes: ${seconds} Seconds` } 
+               </Card.Subtitle>
+            </Card.Body> 
+        </Card>
+    )
+};
+
+export default Clock;
