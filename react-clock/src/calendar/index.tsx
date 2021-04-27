cdc5cf2c24a88ed5f8adbb6752024022bcfdc7ee
Aftabul Islam <aftabiridoy@gmail.com>
2021-04-27 23:27:26 +0600

    add: monthName

--- react-clock/src/calendar/index.tsx
+++ react-clock/src/calendar/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField'
 import moment from 'moment';
-import * as hijri from 'hijri-js'
+import * as hijri from 'hijri-js';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 
 
@@ -10,19 +11,26 @@ const formateHijriDate = (date : any) =>{
     let hijriDate = hijriConverter.toHijri(moment(date).format('DD-MM-YYYY'), '-');
     console.log("hijriDate",hijriDate);
     return `${hijriDate.year}-0${hijriDate.month}-${hijriDate.day}`;
-}
+};
+
+const formatCardHijrimonth = (date : any) =>{
+    let hijriConverter = hijri.initialize();
+    let hijriDate = hijriConverter.toHijri(moment(date).format('DD-MM-YYYY'), '-');
+    return `${hijriDate.monthName}`;
+};
 
 const Calendar = () => {
   let date = new Date();
   let formatedDate = moment(date).format('YYYY-MM-DD');
   const [currentDate, setCurrentDate] = useState(formatedDate);
   const [currentHijiriDate, setHijiriCurrentDate] = useState(formateHijriDate(date));
+  const [cradHijriMonth] = useState(formatCardHijrimonth(date));
 
 
   const onDateChange = async (event : any) => {
       await setCurrentDate(event.target.value);
-      await setHijiriCurrentDate(formateHijriDate(currentDate));
-  };
+      await setHijiriCurrentDate(formateHijriDate(currentDate));  
+   };
   
     return(
         <>
@@ -40,6 +48,14 @@ const Calendar = () => {
             type={"date"}
             value={currentHijiriDate}
             />
+            <Card variant='outlined' color='secondary'>
+                <CardContent>
+                    <Typography>
+                        Month name : {cradHijriMonth}
+                    </Typography>
+                </CardContent>
+
+            </Card>
         </>
 
     );
