20300dc0d518a52266a2897049c329a0d370d2fd
rabbykabir <rabbykabir221@gmail.com>
2021-04-28 03:34:20 +0600

    deleted some unnecessary line

--- react-clock/src/App.tsx
+++ react-clock/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SimpleCard from "./onCard";
-import {Grid} from '@material-ui/core'; 
 
 function App() {
   return (
--- react-clock/src/calendar/index.tsx
+++ react-clock/src/calendar/index.tsx
@@ -2,25 +2,7 @@ import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField'
 import moment from 'moment';
 import * as hijri from 'hijri-js'
-import { makeStyles } from '@material-ui/core/styles';
 
-
-const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
 const formateHijriDate = (date : any) =>{
     let hijriConverter = hijri.initialize();
     let hijriDate = hijriConverter.toHijri(moment(date).format('DD-MM-YYYY'), '-');
